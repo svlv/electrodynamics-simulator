e9a5038245aaa4b3fe34bf21ec72123fffd53ae9
Andrey Suvalov <suvalov.andrew@gmail.com>
2021-03-17 20:55:13 +0300

    Added canvas class

---
+++ src/Canvas.hxx
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <gtkmm/drawingarea.h>
+#include "Charge.hxx"
+
+namespace maxwell
+{
+
+class Canvas : public Gtk::DrawingArea
+{
+public:
+    Canvas();
+    virtual ~Canvas() = default;
+protected:
+    bool on_draw(const Cairo::RefPtr<Cairo::Context>& cr) override;
+
+    bool on_button_press_event(GdkEventButton* event) override;
+    bool on_key_press_event(GdkEventKey* event) override;
+private:
+    Charges _charges;
+    bool _draw_lines = true;
+};
+
+} // namespace maxwell
