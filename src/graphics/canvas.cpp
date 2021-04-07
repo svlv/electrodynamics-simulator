6c81399e7bf4efa871ccbe01f9bee3953e45c81b
Andrey Suvalov <suvalov.andrew@gmail.com>
2021-04-07 09:23:56 +0300

    Refactoring

--- src/graphics/canvas.cpp
+++ src/graphics/canvas.cpp
@@ -10,14 +10,15 @@
 namespace maxwell
 {
 
-class context_guard {
+class context_guard
+{
   public:
-    explicit context_guard(const Cairo::RefPtr<Cairo::Context>& cr) : _cr(cr) {
-      _cr->save();
+    explicit context_guard(const Cairo::RefPtr<Cairo::Context>& cr) : _cr(cr)
+    {
+        _cr->save();
     }
-    ~context_guard() {
-      _cr->restore();
-    }
+    ~context_guard() { _cr->restore(); }
+
   private:
     const Cairo::RefPtr<Cairo::Context>& _cr;
 };
